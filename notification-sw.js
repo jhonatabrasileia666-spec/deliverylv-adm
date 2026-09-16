96c1cb36c66c146f92b6e15ba7eb6eecbb678cb2
jhonatabrasileia666-spec <jhonata.brasileia666@gmail.com>
2026-09-16 10:26:01 -0400

    feat: add notification service worker

---
+++ notification-sw.js
@@ -0,0 +1,10 @@
+self.addEventListener('install',event=>self.skipWaiting());
+self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
+self.addEventListener('notificationclick',event=>{
+  event.notification.close();
+  event.waitUntil((async()=>{
+    const windows=await self.clients.matchAll({type:'window',includeUncontrolled:true});
+    if(windows.length){await windows[0].focus();return}
+    await self.clients.openWindow('./');
+  })());
+});
